9ce79ba0f6481bb224d403d8407ff0e901c5d020
Lahesh <laheshk@gmail.com>
2024-03-21 10:23:24 +0000

    Gradient adapts to user pref theme

--- Carousel/ProgressiveBlurView.swift
+++ Carousel/ProgressiveBlurView.swift
@@ -11,13 +11,17 @@ import Combine
 
 
 struct ProgressiveBlurComponent: View {
+    @Environment(\.colorScheme) var colorScheme
     var body: some View {
         ZStack {
             Color.clear
                 
                 .frame(height: 200)
                 .background(
-                    LinearGradient(gradient: Gradient(colors: [Color.black.opacity(1), Color.clear]), startPoint: .top, endPoint: .bottom)
+                    LinearGradient(gradient: Gradient(colors: [
+                        colorScheme == .dark ? Color.black.opacity(1) : Color.white.opacity(1),
+                        Color.clear
+                    ]), startPoint: .top, endPoint: .bottom)
                 )
 //                .blur(radius: 10)
         }
